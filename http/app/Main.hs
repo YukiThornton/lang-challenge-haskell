1297fd0919b7142c2b0c3a8e2ff3b3e4e69b4e90
YukiThornton <yukiokuno16@gmail.com>
2024-03-10 18:24:51 +0900

    change http string to text

--- http/app/Main.hs
+++ http/app/Main.hs
@@ -1,35 +1,36 @@
 {-# LANGUAGE DeriveGeneric #-}
-{-# LANGUAGE DataKinds #-}
 {-# LANGUAGE OverloadedStrings #-}
 
 module Main where
 
 import Network.HTTP.Req
 import Control.Monad.IO.Class (liftIO)
-import HaskellSay(haskellSay)
 import GHC.Generics
 import Data.Aeson
+import qualified Data.Text as T
+import Prelude hiding (id)
+import qualified Data.Text.IO as DTI (putStrLn)
 
 main :: IO ()
 main = do
     runReq defaultHttpConfig $ do
         res <- req GET
-                   (https "jsonplaceholder.typicode.com" /: "posts" /~ (1 :: Int))
+                   (https "jsonplaceholder.typicode.com" /: "posts")
                    NoReqBody
                    jsonResponse
                    mempty
-        let post = (responseBody res :: Post)
-        liftIO $ haskellSay $ getPostText post
+        let post = (responseBody res :: [Post])
+        liftIO $ DTI.putStrLn $ T.intercalate "\n" $ map getPostText $ take 5 post
         where
-            getPostText :: Post -> String
-            getPostText post = title post <> ": " <> body post
+            getPostText :: Post -> T.Text
+            getPostText post = "Id(" <> T.pack (show (id post)) <> "), Title(" <> title post <> "), Body(" <> T.take 20 (body post) <> "...)"
 
 
 data Post = Post
   { userId :: Int,
     id :: Int,
-    title :: String,
-    body :: String
+    title :: T.Text,
+    body :: T.Text
   } deriving (Show, Generic)
 
 instance FromJSON Post
